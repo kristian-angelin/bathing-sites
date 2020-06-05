4c4336f270f3b18b7dab579aa41fd18949a592a3
Kristian Angelin <kran1800@student.miun.se>
2020-06-06 01:09:01 +0200

    Added dialog showing random bathingsite.

--- app/src/main/java/se/miun/kran1800/dt031g/bathingsites/BathingSiteDao.java
+++ app/src/main/java/se/miun/kran1800/dt031g/bathingsites/BathingSiteDao.java
@@ -6,13 +6,19 @@ import androidx.room.Query;
 
 @Dao
 public interface BathingSiteDao {
+    // Add bathing site to database.
     @Insert
     public void insertBathingSite(BathingSite bathingSite);
 
+    // Get total number of bathing sites in DB.
     @Query("SELECT COUNT(*) FROM bathing_site")
     public int getTotalBathingSites();
 
+    // Check if bathing site already exist by comparing lat/lon.
     @Query("SELECT COUNT(*) FROM bathing_site WHERE latitude = :lat AND longitude = :lon")
     public int checkForDuplicate(double lat, double lon);
 
+    // Return a random bathing site.
+    @Query("SELECT * FROM bathing_site ORDER BY RANDOM() LIMIT 1")
+    public BathingSite getRandomBathingSite();
 }

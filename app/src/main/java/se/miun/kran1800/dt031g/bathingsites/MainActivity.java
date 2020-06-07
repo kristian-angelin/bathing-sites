13952a51a1c2e48fbe345df13af43e485610176e
Kristian Angelin <kran1800@student.miun.se>
2020-06-07 02:19:29 +0200

    Added permission check to access location.

--- app/src/main/java/se/miun/kran1800/dt031g/bathingsites/MainActivity.java
+++ app/src/main/java/se/miun/kran1800/dt031g/bathingsites/MainActivity.java
@@ -1,14 +1,21 @@
 package se.miun.kran1800.dt031g.bathingsites;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.util.Log;
 import android.view.MenuInflater;
@@ -19,6 +26,8 @@ import android.view.Menu;
 
 // TODO: Go through and check if any public things can be set private instead.
 public class MainActivity extends AppCompatActivity {
+
+    final int ACCESS_LOCATION_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,8 +67,52 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    // Try to start map activity by checking permissions first.
     public void startMapActivity (MenuItem menuItem) {
-        Intent intent = new Intent(this, MapsActivity.class);
-        startActivity(intent);
+        // If permission has been granted
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Intent intent = new Intent(this, MapsActivity.class);
+            startActivity(intent);
+        } else {
+            requestLocationPermission();
+        }
+    }
+
+    // Request location permission
+    private void requestLocationPermission() {
+        // If permission has been denied before, show dialog with explanation.
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            new AlertDialog.Builder(this)
+                    .setTitle(getString(R.string.title_permission_required))
+                    .setMessage(getString(R.string.message_location_required))
+                    .setPositiveButton(getString(R.string.button_ok), new DialogInterface.OnClickListener() {
+                        // Click ok to make request again.
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_LOCATION_CODE);
+                        }
+                    })
+                    .setNegativeButton(getString(R.string.button_cancel), new DialogInterface.OnClickListener() {
+                        // Click cancel to dismiss dialog.
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).create().show();
+        }
+        // Ask for permission.
+        else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_LOCATION_CODE);
+        }
+    }
+
+    // Check result of permissions dialog
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == ACCESS_LOCATION_CODE && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Intent intent = new Intent(this, MapsActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/se/miun/kran1800/dt031g/bathingsites/MapsActivity.java
+++ app/src/main/java/se/miun/kran1800/dt031g/bathingsites/MapsActivity.java
@@ -10,6 +10,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -39,10 +40,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setMyLocationEnabled(true);
     }
 }
